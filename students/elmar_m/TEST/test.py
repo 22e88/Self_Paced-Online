8516ebb7f098ff7778b986a76709ba6ca54023dc
22e88 <22e88@mailbox.org>
2018-02-09 18:17:39 +0100

    test.py: added recursive function tests...

---
+++ students/elmar_m/TEST/test.py
@@ -0,0 +1,21 @@
+
+
+def countdown(n):
+    if n == 0:
+        print('bumm!')
+    else:
+        print(n)
+        countdown(n-1)
+    return
+
+def fib(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fib(n-1) + fib(n-2)
+
+
+if __name__ == '__main__':
+    print('please import me...')
